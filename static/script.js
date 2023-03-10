71ad6ec6025d8afca5210dad42d17fd445896428
tari777 <matheusalisauska@gmail.com>
2023-03-10 16:24:28 -0300

    Portfolio e Contato

--- static/script.js
+++ static/script.js
@@ -10,3 +10,18 @@ menuMobile.addEventListener('click', () => {
     : menuMobile.classList.replace("bi-x", "bi-list");
     body.classList.toggle("menu-nav-active")
 });
+
+/* Fecha o menu quando clicar em algum item, e muda o icon para list*/
+
+const navItem = document.querySelectorAll('.nav-item')
+
+console.log(navItem)
+
+navItem.forEach(item => {
+  item.addEventListener("click", () => {
+    if (body.classList.contains("menu-nav-active")) {
+      body.classList.remove("menu-nav-active")
+      menuMobile.classList.replace("bi-x", "bi-list");
+    }
+  })
+})
